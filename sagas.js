4252ea81fb364e9dbd0c3cdf68301195497cf7e3
Ben Hulan <ben.hulan@bounteous.com>
2020-03-03 16:01:17 -0600

    Add example from YouTube, with test

--- sagas.js
+++ sagas.js
@@ -2,8 +2,18 @@ import { put, takeEvery, all, call } from 'redux-saga/effects'
 
 export const delay = (ms) => new Promise(res => setTimeout(res, ms))
 
-function* helloSaga() {
-  console.log('Hello Sagas!')
+export function* helloSaga() {
+  // interesting example from YouTube: https://www.youtube.com/watch?v=o3A9EvMspig
+  let x = yield 'Please give me a value for x'
+  console.log(x)
+
+  let y = yield 'Please give me a value for y'
+  console.log(y)
+
+  let z = yield 'Please give me a value for z'
+  console.log(z)
+
+  return (x + y + z)
 }
 
 // worker saga performs the async increment task
--- sagas.spec.js
+++ sagas.spec.js
@@ -1,7 +1,7 @@
 import test from 'tape';
 
 import { put, call } from 'redux-saga/effects'
-import { incrementAsync, delay } from './sagas'
+import { incrementAsync, delay, helloSaga } from './sagas'
 
 test('incrementAsync Saga test', (assert) => {
   const gen = incrementAsync()
@@ -22,6 +22,36 @@ test('incrementAsync Saga test', (assert) => {
     gen.next(),
     { done: true, value: undefined },
     'incrementAsync Saga must be done'
+  )
+
+  assert.end()
+});
+
+test('helloSaga Saga test', (assert) => {
+  const gen = helloSaga()
+
+  assert.deepEqual(
+    gen.next().value,
+    'Please give me a value for x',
+    'helloSaga Saga needs a value for x. Passing 4'
+  )
+
+  assert.deepEqual(
+    gen.next(4).value,
+    'Please give me a value for y',
+    'helloSaga Saga needs a value for y. Passing 5'
+  )
+
+  assert.deepEqual(
+    gen.next(5).value,
+    'Please give me a value for z',
+    'helloSaga Saga needs a value for z. Passing 6'
+  )
+
+  assert.deepEqual(
+    gen.next(6),
+    { done: true, value: 15 },
+    'helloSaga final value will be 4 + 5 + 6 = 15'
   )
 
   assert.end()
